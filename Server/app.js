9a2f5190751ed3dadb9a3b3d45f45bd4c94098d9
Toussaint <estoussaint@labs.neit.edu>
2018-02-22 14:57:03 -0500

    Aj Network Movement

---
+++ Server/app.js
@@ -0,0 +1,32 @@
+var io = require('socket.io')(process.envPort||3000);
+var shortid = require('shortid');
+
+console.log("Server Started");
+//console.log(shortid.generate());
+
+var players = [];
+
+io.on('connection', function(socket){
+	var thisPlayerId = shortid.generate();
+	console.log('client connected spawmning player id: ', thisPlayerId);
+	
+	socket.broadcast.emit('spawn player', {id:thisPlayerId});
+	
+	players.forEach(function(playerId){
+		if(playerId == thisPlayerId) return;
+		socket.emit('spawn player', {id:playerId});
+		console.log("Adding a new player", playerId);
+	});
+	
+	socket.on('move', function(data){
+		data.id = thisPlayerId;
+		console.log("Player position is: ", JSON.stringify(data));
+		socket.broadcast.emit('move', data);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log("Player Disconnected");
+		players.splice(players.indexOf(thisPlayerId), 1);
+		socket.broadcast.emit('disconnected', {id:thisPlayerId});
+	});
+});
---
+++ SocketIOProject/SocketIOProject/Assets/Network.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+	static SocketIOComponent socket;
+	public GameObject playerPrefab;
+
+    Dictionary<string, GameObject> players;
+
+	// Use this for initialization
+	void Start () {
+		socket = GetComponent<SocketIOComponent> ();
+		socket.On ("open", OnConnected);
+		socket.On ("spawn player", OnSpawned);
+        socket.On("disconnected", OnDisconnected);
+        socket.On("move", OnMove);
+        players = new Dictionary<string, GameObject>();
+	}
+	
+	// Tells us we are connected
+	void OnConnected (SocketIOEvent e) {
+		Debug.Log ("We are Connected");
+		socket.Emit ("playerhere");
+	}
+
+	void OnSpawned(SocketIOEvent e){
+		Debug.Log ("Player Spawned!" + e.data);
+		var player = Instantiate (playerPrefab);
+        players.Add(e.data["id"].ToString(), player);
+        Debug.Log("Count " + players.Count);
+	}
+
+    void OnDisconnected (SocketIOEvent e)
+    {
+        Debug.Log("player disconnected " + e.data);
+        var id = e.data["id"].ToString();
+        var player = players[id];
+        Destroy(player);
+        players.Remove(id);
+    }
+
+    void OnMove(SocketIOEvent e)
+    {
+        Debug.Log("Networked player is moving " + e.data);
+
+        var id = e.data["id"].ToString();
+        var player = players[id]; 
+        Debug.Log(id);
+        var pos = new Vector3(GetFloatFromJson(e.data, "x"), 0,GetFloatFromJson(e.data, "y"));
+        Debug.Log("pos: " + pos);
+        var netMove = player.GetComponent<CharacterMovement>();
+
+        //netMove.NetworkMovement();
+    }
+
+    float GetFloatFromJson(JSONObject data, string key)
+    {
+        return float.Parse(data[key].ToString().Replace("\"", ""));
+    }
+}
